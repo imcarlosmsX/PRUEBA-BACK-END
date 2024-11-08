80fb1df1bccc24ddab16f8c7634f39781b4ae465
imcarlosmsX <sandovalcarlos@uninorte.edu.co>
2024-11-08 00:44:26 -0500

    Update database connection string in settings.py to point to 'scrumback' database

--- scrumback/settings.py
+++ scrumback/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'scrumback.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/scrumback',
         conn_max_age=600
     )
 }
